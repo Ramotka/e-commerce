074f1bcf5a1ef66fde3dc97cbc9d352f246b30ef
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-06-17 21:36:08 +0200

    Angular Material installed + admin-add-product

---
+++ projects/product/src/lib/adapters/primary/components/admin-add-product.component-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminAddProductComponent } from './admin-add-product.component';
+
+@NgModule({ imports: [CommonModule],
+  	declarations: [AdminAddProductComponent],
+  	providers: [],
+  	exports: [AdminAddProductComponent] })
+export class AdminAddProductComponentModule {
+}
---
+++ projects/product/src/lib/adapters/primary/components/admin-add-product.component.ts
@@ -0,0 +1,5 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({ selector: 'lib-admin-add-product', templateUrl: './admin-add-product.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class AdminAddProductComponent {
+}
