023c9a9cded9b5f5f70777362bcef3100b56bb97
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-06-17 21:25:49 +0200

    lib product + admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
+import { AdminPageModule } from './pages/admin.page-module';
 
 const routes: Routes = [{ 
         path: '', 
         loadChildren: () => HomePageModule
-      }];
+      },
+  { 
+        path: 'admin', 
+        loadChildren: () => AdminPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
